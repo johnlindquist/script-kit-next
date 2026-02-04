877be6a8445cdc1c34a14a63fc0d6688e1f4be29
John Lindquist <johnlindquist@gmail.com>
2026-02-04 02:20:34 -0700

    [xloop] test: validate random builtin action/dialog behaviors with 161 tests (batch 9)

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -126,6 +126,10 @@ mod dialog_builtin_action_validation_tests_7;
 mod dialog_builtin_action_validation_tests_8;
 
 #[cfg(test)]
+#[path = "dialog_builtin_action_validation_tests_9.rs"]
+mod dialog_builtin_action_validation_tests_9;
+
+#[cfg(test)]
 mod tests {
     // Import from submodules directly - these are only used in tests
     use super::builders::{get_global_actions, get_script_context_actions};
