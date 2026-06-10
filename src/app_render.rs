7805d40602b70f3af88f35e039103b7db17222c0
John Lindquist <johnlindquist@gmail.com>
2026-06-09 23:30:34 -0600

    Delete dead group-header preview renderer
    render_group_header_preview was an unused #[allow(dead_code)] mock
    with 36 hardcoded 140x28 tiles; the live design gallery renders group
    header styles through group_header_item instead.

--- src/app_render.rs
+++ src/app_render.rs
@@ -2,7 +2,6 @@
 include!("app_render/preview_panel.rs");
 include!("app_render/focused_info.rs");
 include!("app_render/preview_metadata.rs");
-include!("app_render/group_header_preview.rs");
 include!("app_render/group_header_item.rs");
 include!("app_render/calculator_item.rs");
 include!("app_render/tests.rs");
